fe385d51744e3af8cded17f355332154fd9fd8c9
Manuel <jirsakm@web.de>
2020-08-09 14:37:03 +0200

    Removed debug leftovers

--- src/main/java/net/enderitemc/enderitemod/init/EnderiteModConfig.java
+++ src/main/java/net/enderitemc/enderitemod/init/EnderiteModConfig.java
@@ -28,8 +28,6 @@ public class EnderiteModConfig {
     public static ForgeConfigSpec.IntValue ENDERITE_TOP_OFFSET;
     public static ForgeConfigSpec.IntValue ENDERITE_MAXIMUM;
 
-    private static final Logger LOGGER = LogManager.getLogger();
-
     static {
         COMMON_BUILDER.comment("General settings").push(CATEGORY_GENERAL);
         //currently unused, but general settings config goes in here
@@ -47,13 +45,9 @@ public class EnderiteModConfig {
     private static void setupEnderiteGeneration() {
         COMMON_BUILDER.comment("Enderite settings").push(SUBCATEGORY_ENDERITE_GENERATION);
         ENDERITE_COUNT = COMMON_BUILDER.comment("How many enderite ore should be generated per chunk (default: 3)").defineInRange("count",3,1,255);
-        LOGGER.info("Enderite Count is set to "+ENDERITE_COUNT);
         ENDERITE_BOTTOM_OFFSET = COMMON_BUILDER.comment("Bottom offset for enderite ore generation, e.g. if you choose this value as '12' (default value), ore will only be generated above y = 12").defineInRange("bottom_offset",12,0,255);
-        LOGGER.info("Enderite bottom offset is set to "+ENDERITE_BOTTOM_OFFSET);
         ENDERITE_TOP_OFFSET = COMMON_BUILDER.comment("Top offset for enderite ore generation, based on maximum height, e.g. if you choose this value as '12' (default value), ore will only be generated below y = maximum - 12").defineInRange("top_offset",12,0,255);
-        LOGGER.info("Enderite top offset set to "+ENDERITE_TOP_OFFSET);
         ENDERITE_MAXIMUM = COMMON_BUILDER.comment("Maximum height for enderite ore generation, has to be greater than 'top_offset', default value is '48'").defineInRange("maximum", 48,1,255);
-        LOGGER.info("Enderite maximum is set to "+ENDERITE_MAXIMUM);
     }
 
     public static void loadConfig(ForgeConfigSpec spec, Path path) {
