b86863fe293cfebfadd4f14eb91adf9efdb79bef
Pitti11 <>
2020-11-01 13:11:24 +0100

    implemented fix for ore spawning

--- src/main/java/net/enderitemc/enderitemod/init/WorldFeatures.java
+++ src/main/java/net/enderitemc/enderitemod/init/WorldFeatures.java
@@ -8,9 +8,11 @@ import net.enderitemc.enderitemod.world.gen.BiomeHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.template.BlockMatchRuleTest;
@@ -37,13 +39,17 @@ public class WorldFeatures {
                                 + ", " + maximum);
                 TopSolidRangeConfig countConfig = new TopSolidRangeConfig(bottomOffset, topOffset, maximum);
 
+                ConfiguredFeature<?, ?> enderite_cf = Feature.ORE
+                                .withConfiguration(new OreFeatureConfig(fillerBlockType, state, 3))
+                                .withPlacement(Placement.field_242907_l.configure(countConfig)).func_242728_a()
+                                .func_242731_b(count);
+
+                Registry.register(WorldGenRegistries.field_243653_e,
+                                new ResourceLocation(EnderiteMod.MOD_ID, "enderite_cf"), enderite_cf);
                 WorldGenRegistries.field_243657_i.forEach(biome -> {
                         if (biome.getCategory() == Biome.Category.THEEND) {
                                 BiomeHandler handler = new BiomeHandler(biome);
-                                handler.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE
-                                                .withConfiguration(new OreFeatureConfig(fillerBlockType, state, 3))
-                                                .withPlacement(Placement.field_242907_l.configure(countConfig))
-                                                .func_242728_a().func_242731_b(count));
+                                handler.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, enderite_cf);
                         }
                 });
         }
