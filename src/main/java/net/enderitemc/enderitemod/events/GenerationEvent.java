51fca3bbe2afcf45a01bc8db77ef3639fec43c92
Nic4Las <>
2023-01-02 00:41:57 +0100

    Update to 1.18.2

--- src/main/java/net/enderitemc/enderitemod/events/GenerationEvent.java
+++ src/main/java/net/enderitemc/enderitemod/events/GenerationEvent.java
@@ -5,6 +5,9 @@ import net.enderitemc.enderitemod.init.EnderiteModConfig;
 import net.enderitemc.enderitemod.init.Registration;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
@@ -16,6 +19,7 @@ import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
@@ -30,14 +34,16 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber
 public class GenerationEvent {
 
     //private static final ArrayList<ConfiguredFeature<?, ?>> enderOres = new ArrayList<ConfiguredFeature<?, ?>>();
-    private static final ArrayList<PlacedFeature> enderOres = new ArrayList<PlacedFeature>();
+    private static final ArrayList<Holder<PlacedFeature>> enderOres = new ArrayList<Holder<PlacedFeature>>();
 
     public static void registerOres() {
         int veinsize = EnderiteModConfig.ENDERITE_VEIN_SIZE.get()+2;
@@ -45,13 +51,24 @@ public class GenerationEvent {
         int bottomOffset = EnderiteModConfig.ENDERITE_BOTTOM_OFFSET.get();
         int topOffset = EnderiteModConfig.ENDERITE_TOP_OFFSET.get();
         int maximum = EnderiteModConfig.ENDERITE_MAXIMUM.get();
-        ConfiguredFeature<?,?> configFeature = /*register("end_enderite_ore", */Feature.ORE
-                .configured(new OreConfiguration(new TagMatchTest(Tags.Blocks.END_STONES),
-                        Registration.ENDERITE_ORE.get().defaultBlockState(), veinsize));
-                // .m_7679_(FeatureDecorator.f_70692_.m_70720_(new RangeDecoratorConfiguration(UniformHeight.of(VerticalAnchor.absolute(bottomOffset), VerticalAnchor.absolute(maximum-topOffset)))))
-                // .m_64152_() // spawn height
-                // .m_64158_(count))); // spawn frequency per chunks
-        enderOres.add(registerPlacedFeature("end_enderite_ore",configFeature,CountPlacement.of(count),InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.absolute(bottomOffset), VerticalAnchor.absolute(maximum-topOffset)), BiomeFilter.biome()));
+
+        RuleTest END_STONE = new TagMatchTest(Tags.Blocks.END_STONES);
+        OreConfiguration EnderiteOreConfig = new OreConfiguration(END_STONE, Registration.ENDERITE_ORE.get().defaultBlockState(), veinsize);
+        Holder<ConfiguredFeature<OreConfiguration, ?>> configFeature = FeatureUtils.register("end_enderite_ore", Feature.ORE, EnderiteOreConfig);
+        Holder<PlacedFeature> ORE_ENDERITE = PlacementUtils.register("end_enderite_ore", configFeature, InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.absolute(bottomOffset), VerticalAnchor.absolute(maximum-topOffset)), BiomeFilter.biome());
+        enderOres.add(ORE_ENDERITE);
+        // Holder<PlacedFeature> ORE_ANCIENT_DEBRIS_SMALL = PlacementUtils.register("ore_debris_small", OreFeatures.ORE_ANCIENT_DEBRIS_SMALL, InSquarePlacement.spread(), PlacementUtils.RANGE_8_8, BiomeFilter.biome());
+        
+        
+        //         .configured(new OreConfiguration(new TagMatchTest(Tags.Blocks.END_STONES),
+        //                 Registration.ENDERITE_ORE.get().defaultBlockState(), veinsize));
+        // enderOres.add(registerPlacedFeature("end_enderite_ore",
+        // configFeature,
+        // CountPlacement.of(count),
+        // InSquarePlacement.spread(), 
+        // HeightRangePlacement.uniform(VerticalAnchor.absolute(bottomOffset), 
+        // VerticalAnchor.absolute(maximum-topOffset)), BiomeFilter.biome()));
+        
         
     }
 
@@ -59,23 +76,23 @@ public class GenerationEvent {
     public static void gen(BiomeLoadingEvent event) {
         BiomeGenerationSettingsBuilder generation = event.getGeneration();
         if (event.getCategory().equals(Biome.BiomeCategory.THEEND)) {
-            for (PlacedFeature ore : enderOres) {
+            for (Holder<PlacedFeature> ore : enderOres) {
                 if (ore != null)
                     generation.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, ore);
             }
         }
     }
 
-    private static <FC extends FeatureConfiguration> ConfiguredFeature<FC, ?> register(String name,
-            ConfiguredFeature<FC, ?> configureFeature) {
-        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
-                new ResourceLocation(EnderiteMod.MOD_ID + ":" + name), configureFeature);
-    }
+    // private static <FC extends FeatureConfiguration> ConfiguredFeature<FC, ?> register(String name,
+    //         ConfiguredFeature<FC, ?> configureFeature) {
+    //     return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
+    //             new ResourceLocation(EnderiteMod.MOD_ID + ":" + name), configureFeature);
+    // }
 
-    private static <C extends FeatureConfiguration, F extends Feature<C>> PlacedFeature registerPlacedFeature(String registryName,
-               ConfiguredFeature<C, F> feature, PlacementModifier... placementModifiers) {
-        PlacedFeature placed = BuiltinRegistries.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(EnderiteMod.MOD_ID + ":" + registryName), feature)
-               .placed(placementModifiers);
-        return PlacementUtils.register(registryName, placed);
-    }
+    // private static <C extends FeatureConfiguration, F extends Feature<C>> PlacedFeature registerPlacedFeature(String registryName,
+    //            ConfiguredFeature<C, F> feature, PlacementModifier... placementModifiers) {
+    //     PlacedFeature placed = BuiltinRegistries.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(EnderiteMod.MOD_ID + ":" + registryName), feature)
+    //            .placed(placementModifiers);
+    //     return PlacementUtils.register(registryName, placed);
+    // }
 }
--- src/main/java/net/enderitemc/enderitemod/misc/EnderiteTag.java
+++ src/main/java/net/enderitemc/enderitemod/misc/EnderiteTag.java
@@ -1,15 +1,17 @@
 package net.enderitemc.enderitemod.misc;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.tags.Tag;
-import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 
 public class EnderiteTag {
-        public static final Tag<Item> ENDERITE_ITEM = ItemTags.getAllTags()
-                        .getTagOrEmpty(new ResourceLocation("enderitemod", "enderite_items"));
-        public static final Tag<Item> CRAFTABLE_SHULKER_BOXES = ItemTags.getAllTags()
-                        .getTagOrEmpty(new ResourceLocation("enderitemod", "shulker_boxes"));
-        public static final Tag<Item> ENDERITE_ELYTRAS = ItemTags.getAllTags()
-                        .getTagOrEmpty(new ResourceLocation("enderitemod", "enderite_elytras"));
+        public static final TagKey<Item> ENDERITE_ITEM = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("enderitemod", "enderite_items"));
+        public static final TagKey<Item> CRAFTABLE_SHULKER_BOXES = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("enderitemod", "shulker_boxes"));
+        public static final TagKey<Item> ENDERITE_ELYTRAS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("enderitemod", "enderite_elytras"));
+        //                 .getTagOrEmpty(new ResourceLocation("enderitemod", "enderite_items"));
+        // public static final TagKey<Item> CRAFTABLE_SHULKER_BOXES = ItemTags.getAllTags()
+        //                 .getTagOrEmpty(new ResourceLocation("enderitemod", "shulker_boxes"));
+        // public static final TagKey<Item> ENDERITE_ELYTRAS = ItemTags.getAllTags()
+        //                 .getTagOrEmpty(new ResourceLocation("enderitemod", "enderite_elytras"));
 }
